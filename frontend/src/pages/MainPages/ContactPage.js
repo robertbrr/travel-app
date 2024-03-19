4061899409fd0187e542e254bfdacbdd7197338a
robertbrinzoi <rorakex@icloud.com>
2024-03-19 23:16:29 +0200

    added ContactPage
    TODO:
    	destination grid
    	login + register

--- frontend/src/pages/MainPages/ContactPage.js
+++ frontend/src/pages/MainPages/ContactPage.js
@@ -12,11 +12,11 @@ function ContactPage()
         <div className = "destinations-page">
             <MainSidebar></MainSidebar>
             <div className="contact-container">
-                <img src={ companyLogo } style={ { width: '100%', height: 'auto',  transform: "scale(0.5)" } } alt='Globert Logo'/>
-                <div style = { { paddingTop: "40px" } } > Phone: 0766121009</div>
+                <img src = { companyLogo } style={ { width: '100%', height: 'auto',  transform: "scale(0.5)" } } alt = 'Globert Logo'/>
+                <div style = { { paddingTop: "30px" } } > Phone: 0766121009</div>
                 <div> Mail: globert_travel@travel.ro</div>
                 <div> Address: 20045 Hilpert View, East Kari, Nevada</div>
-                <div> Business Hours: 10am - 6pm on weekdays, closed in the weekends </div>
+                <div> Business Hours: 10am-6pm on weekdays, closed in the weekends </div>
             </div>
         </div>
     );
